4859408442483674b74fb34d226f42547092eac1
ilham <ntdy10@gmail.com>
2023-02-06 13:28:27 +0100

    Update streamlit_app.py
    add fruit macros from panda df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My parents healthy diner')
 
 streamlit.header('Breakfast Fav')
@@ -9,3 +11,4 @@ streamlit.text('🐔 Hard Boiled Free range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
